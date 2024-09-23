cb9580ee26170344e76ffad1c4712546ce9a6bb6
scaceres5 <scaceres5@cuc.edu.co>
2024-09-23 09:48:25 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import miLogo from "./assets/img/ojo.png"
+
+function App() {
+
+  return (
+    <>
+     <img src={miLogo} alt="Logo React" />
+     <h1>¡¡ Hola!!</h1>
+     <p>Aqui esta el segundo texto</p>
+    </>
+  )
+}
+
+export default App
+
